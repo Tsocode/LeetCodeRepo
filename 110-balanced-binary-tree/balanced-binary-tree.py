58e2c738e4601e234c6362b92ea10d0a3cd0a45e
Atsagbede Omaretsoguwa <67780220+Tsocode@users.noreply.github.com>
2024-05-30 07:19:52 -0400

    Time: 50 ms (31.49%) | Memory: 17.4 MB (99.36%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -5,11 +5,13 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def isBalanced(self, root):
-        return (self.Height(root) >= 0)
-    def Height(self, root):
-        if root is None:  return 0
-        leftheight, rightheight = self.Height(root.left), self.Height(root.right)
-        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1
-        return max(leftheight, rightheight) + 1
+    def isBalanced(self, root):  # Defines the isBalanced method
+        return (self.Height(root) >= 0)  # Checks if the tree is balanced by calling Height method
+    
+    def Height(self, root):  # Defines the Height method
+        if root is None:  return 0  # Returns 0 if the tree is empty
+        leftheight, rightheight = self.Height(root.left), self.Height(root.right)  # Calculates heights of left and right subtrees
+        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1  # Checks if the tree is unbalanced
+        return max(leftheight, rightheight) + 1  # Returns the height of the tree if balanced
+
         
